1f85c9a27c150e65269389e65abc7f4c5927a0c6
aaronchang.tw <aaronchang.tw@gmail.com>
2024-01-27 19:13:14 -0500

    fix image name

--- clean_run.sh
+++ clean_run.sh
@@ -10,4 +10,4 @@ docker image rm $(docker image ls | grep 'ubuntu' | awk '{print $3}')
 
 docker build -t py3_jupyter .
 
-docker run -itd -p8888:8888 --rm -v"$PWD":/home/app py3_jupyter
+docker run --rm -itd --gpus all -p 8888:8888 -v $(pwd)/docker-data:/content py3_jupyter jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$gu1oaVTudqMVaMY+ufyldg$dXMYv+IMfcsfNv9ZiEReHp4KoXEb0bW0o8qYFUU13hg
